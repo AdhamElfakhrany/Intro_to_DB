03aaa94f88018bc3fef22d8b8091bc0d68c078a3
adham elfakhrany <54482105+AdhamElfakhrany@users.noreply.github.com>
2025-08-17 03:04:52 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,41 +1,23 @@
--- Create the Authors table (must come first as Books references it)
-CREATE TABLE IF NOT EXISTS authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
-);
+import mysql.connector
 
--- Create the Books table
-CREATE TABLE IF NOT EXISTS books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE NOT NULL,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
-);
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='Salma.MySQL.21',
+        database='alx_book_store'
+    )
+    cursor = connection.cursor()
+    create_db = "CREATE DATABASE IF NOT EXISTS alx_book_store;"
+    cursor.execute(create_db)
 
--- Create the Customers table
-CREATE TABLE IF NOT EXISTS customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE NOT NULL,
-    address TEXT
-);
+except mysql.connector.Error as err:
+    print(f"Something went wrong: {err}")  
 
--- Create the Orders table
-CREATE TABLE IF NOT EXISTS orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
-);
-
--- Create the Order_Details table
-CREATE TABLE IF NOT EXISTS order_details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
-);
+finally:
+    if connection.is_connected():
+        print("Database 'alx_book_store' created successfully!")
+        cursor.close()
+        connection.close()
+    else:
+        print("Connection is not established")
